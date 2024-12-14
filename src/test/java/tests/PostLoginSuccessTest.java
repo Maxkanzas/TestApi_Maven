e114b6728df595dc81484421e0859c2db3a2c23e
maxkanzas <maxkanz1991@gmail.com>
2024-12-14 16:54:18 +0400

    change pomfile and PostLoginSuccessTest

--- src/test/java/tests/PostLoginSuccessTest.java
+++ src/test/java/tests/PostLoginSuccessTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.qameta.allure.*;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -10,11 +11,16 @@ import models.RegisterResponse;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Epic("Работа с данными пользователей")
+@Feature("Получает информацию о пользователях")
 public class PostLoginSuccessTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @Story("Проверяем отправку данных конкретного пользователя")
+    @Severity(SeverityLevel.BLOCKER)
     @ParameterizedTest
     @CsvSource({
             "'eve.holt@reqres.in', 'cityslicka', 'JAPAN', true",
@@ -32,6 +38,7 @@ public class PostLoginSuccessTest {
         registerRequest.setPassword(password);
         registerRequest.setState(state);
 
+        step("Отправляем POST-запрос на регистрацию");
         Response response = RestAssured
                 .given()
                 .contentType(ContentType.JSON)
@@ -43,13 +50,19 @@ public class PostLoginSuccessTest {
                 .response();
 
         if (isSuccess) {
+            step("Проверяем успешную регистрацию, статус код 200");
             assertEquals(200, response.getStatusCode(), "Статус код не соответствует 200");
             RegisterResponse registerResponse = objectMapper.readValue(response.asString(), RegisterResponse.class);
+
+            step("Проверяем, что в ответе присутствует token");
             assertNotNull(registerResponse.getToken(), "Token не должен быть null");
         } else {
+            step("Проверяем неуспешную регистрацию, статус код 400");
             assertEquals(400, response.getStatusCode(), "Статус код не соответствует 400");
             RegisterResponse registerResponse = objectMapper.readValue(response.asString(), RegisterResponse.class);
             String errorMessage = registerResponse.getError();
+
+            step("Проверяем сообщение об ошибке");
             assertNotNull(errorMessage, "Сообщение об ошибке не должно быть null");
             assertEquals("Missing password", errorMessage, "Сообщение об ошибке не совпадает");
         }
